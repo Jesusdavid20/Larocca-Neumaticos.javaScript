36eef49c9343d629d69ea4426ee819d0a15a1849
jesusdavid20 <jesusacostadd24@gmail.com>
2022-08-29 22:54:57 -0300

    funciones

---
+++ js/app.js
@@ -0,0 +1,56 @@
+
+let neumatico1 = 14000;
+let neumatico1Nombre = "195/55R16 Bridgestone EP150";
+
+let neumatico2 = 18000;
+let neumatico2Nombre = "205/55R16 Bridgestone Turanza ER300";
+
+let neumatico3 = 21000;
+let neumatico3Nombre = "195/65R15 Firestone FH900";
+
+//El usuario ingresa que cantidad del neumatico 1 quiere comprar:
+
+let seleccion1 = parseInt(prompt(`el neumatico ${neumatico1Nombre} tiene un precio de ${neumatico1} que cantidad desea?`));
+if (seleccion1 <= 4) {
+    var resultado1 = neumatico1 * seleccion1;
+    var respuesta = prompt(`Usted a seleccionado ${seleccion1} neumaticos y tiene un valor total de ${resultado1}, Desea seguir comprando?`)
+
+    //El usuario ingresa que cantidad del neumatico 2 quiere comprar:
+    if (respuesta = "si") {
+        var seleccion2 = parseInt(prompt(`el neumatico ${neumatico2Nombre} tiene un precio de ${neumatico2} que cantidad desea?`)); 
+        if (seleccion2 <= 4) {
+            var resultado2 = neumatico2 * seleccion2;
+            var respuesta2 = prompt(`Usted a seleccionado ${seleccion2} neumaticos y tiene un valor total de ${resultado2}, desea seguir comprando?`);
+
+            //El usuario ingresa que cantidad del neumatico 3 quiere comprar:
+            if (respuesta2 = "si") {
+                var seleccion3 = parseInt(prompt(`el neumatico ${neumatico3Nombre} tiene un precio de ${neumatico3} que cantidad desea?`)); 
+                if (seleccion3 <= 4) {
+                    var resultado3 = neumatico3 * seleccion3;
+                    alert(`Usted a seleccionado ${seleccion3} neumaticos y tiene un valor total de ${resultado3}`)
+
+                    calculoDelTotal()
+                } else if (seleccion3 > 4) {
+                    alert("No tenemos esa cantidad en stock, por favor intente de nuevo");
+
+                } else if (seleccion2 > 4) {
+                    alert("No tenemos esa cantidad en stock, por favor intente de nuevo")
+                }
+            } else {
+                alert("gracias por su tiempo...")
+            }
+        } else if (seleccion2 > 4) {
+            alert("No tenemos esa cantidad en stock, por favor intente de nuevo")
+        }
+    } else {
+        alert("gracias por su tiempo...")
+    }
+} else if (seleccion1 > 4) {
+    alert("No tenemos esa cantidad en stock, por favor intente de nuevo")
+}
+
+function calculoDelTotal() {
+    let sumaTotal = resultado1 + resultado2 + resultado3;
+    let seleccionTotal = Number( seleccion1 + seleccion3 + seleccion2) ;
+    alert(`Usted a comprado ${seleccionTotal} neumaticos en distintas presentaciones y el total a pagar es ${sumaTotal}`)
+}
