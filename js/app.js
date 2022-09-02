98fcbad04df0bc31209efc381909418e10608502
jesusdavid20 <jesusacostadd24@gmail.com>
2022-09-01 23:51:42 -0300

    Adding objets

--- js/app.js
+++ js/app.js
@@ -1,39 +1,58 @@
 
-let neumatico1 = 14000;
-let neumatico1Nombre = "195/55R16 Bridgestone EP150";
+let neumatico1 = {
+    nombre : "195/55R16 Bridgestone EP150",
+    precio : 14000,
+    stock : 4  
+}
 
-let neumatico2 = 18000;
-let neumatico2Nombre = "205/55R16 Bridgestone Turanza ER300";
+// let neumatico1 = 14000;
+// let stockNeumatico1 =
+// let neumatico1Nombre = "195/55R16 Bridgestone EP150";
 
-let neumatico3 = 21000;
-let neumatico3Nombre = "195/65R15 Firestone FH900";
+let neumatico2 = {
+    nombre : "205/55R16 Bridgestone Turanza ER300",
+    precio : 21000,
+    stock : 8
+}
+
+// let neumatico2 = 18000;
+// let neumatico2Nombre = "205/55R16 Bridgestone Turanza ER300";
+
+let neumatico3 = {
+    nombre : "195/65R15 Firestone FH900",
+    precio : 25000,
+    stock : 5
+}
+
+// let neumatico3 = 21000;
+// let neumatico3Nombre = "195/65R15 Firestone FH900";
 
 //El usuario ingresa que cantidad del neumatico 1 quiere comprar:
 
-let seleccion1 = parseInt(prompt(`el neumatico ${neumatico1Nombre} tiene un precio de ${neumatico1} que cantidad desea?`));
-if (seleccion1 <= 4) {
-    var resultado1 = neumatico1 * seleccion1;
-    var respuesta = prompt(`Usted a seleccionado ${seleccion1} neumaticos y tiene un valor total de ${resultado1}, Desea seguir comprando?`)
+let seleccion1 = parseInt(prompt(`el neumatico ${neumatico1.nombre} tiene un precio de ${neumatico1.precio} que cantidad desea?`));
+if (seleccion1 <= neumatico1.stock) {
+    let resultado1 = neumatico1.precio * seleccion1;
+    let respuesta = prompt(`Usted a seleccionado ${seleccion1} neumaticos y tiene un valor total de ${resultado1}, Desea seguir comprando?`)
 
     //El usuario ingresa que cantidad del neumatico 2 quiere comprar:
     if (respuesta == "si") {
-        var seleccion2 = parseInt(prompt(`el neumatico ${neumatico2Nombre} tiene un precio de ${neumatico2} que cantidad desea?`));
-        if (seleccion2 <= 4) {
-            var resultado2 = neumatico2 * seleccion2;
-            var respuesta2 = prompt(`Usted a seleccionado ${seleccion2} neumaticos y tiene un valor total de ${resultado2}, desea seguir comprando?`);
+        let seleccion2 = parseInt(prompt(`el neumatico ${neumatico2.nombre} tiene un precio de ${neumatico2.precio} que cantidad desea?`));
+        if (seleccion2 <= neumatico2.stock) {
+            let resultado2 = neumatico2.precio * seleccion2;
+            let respuesta2 = prompt(`Usted a seleccionado ${seleccion2} neumaticos y tiene un valor total de ${resultado2}, desea seguir comprando?`);
 
             //El usuario ingresa que cantidad del neumatico 3 quiere comprar:
             if (respuesta2 == "si") {
-                var seleccion3 = parseInt(prompt(`el neumatico ${neumatico3Nombre} tiene un precio de ${neumatico3} que cantidad desea?`));
-                if (seleccion3 <= 4) {
-                    var resultado3 = neumatico3 * seleccion3;
+                let seleccion3 = parseInt(prompt(`el neumatico ${neumatico3.nombre} tiene un precio de ${neumatico3.precio} que cantidad desea?`));
+                if (seleccion3 <= neumatico3.stock) {
+                    let resultado3 = neumatico3.precio * seleccion3;
                     alert(`Usted a seleccionado ${seleccion3} neumaticos y tiene un valor total de ${resultado3}`)
 
                     calculoDelTotal()
-                } else if (seleccion3 > 4) {
+                } else if (seleccion3 > neumatico3.stock) {
                     alert("No tenemos esa cantidad en stock, por favor intente de nuevo");
 
-                } else if (seleccion2 > 4) {
+                } else if (seleccion2 > neumatico2.stock) {
                     alert("No tenemos esa cantidad en stock, por favor intente de nuevo")
                 }
             } else {
@@ -41,7 +60,7 @@ if (seleccion1 <= 4) {
 
                 calculoDelTotal()
             }
-        } else if (seleccion2 > 4) {
+        } else if (seleccion2 > neumatico2.stock) {
             alert("No tenemos esa cantidad en stock, por favor intente de nuevo")
         }
     } else {
@@ -49,7 +68,7 @@ if (seleccion1 <= 4) {
         
         calculoDelTotal()
     }
-} else if (seleccion1 > 4) {
+} else if (seleccion1 > neumatico1.stock) {
     alert("No tenemos esa cantidad en stock, por favor intente de nuevo")
 }
 
