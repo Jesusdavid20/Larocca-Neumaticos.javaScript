6b9031dff873b968cc2d4f28119f5fbe1b818be0
jesusdavid20 <jesusacostadd24@gmail.com>
2022-09-15 23:48:40 -0300

    Logica de la pagina

--- js/app.js
+++ js/app.js
@@ -128,5 +128,25 @@ function calculoDelTotal() {
     }
 }
 */
- let tipoNeumatico = document.querySelector(`#type`)
- console.log(tipoNeumatico)
+
+let stockNeumaticos = [
+    {id: 1,marca: "Bridgestone", modelo:"EP150", medida:"195/55R16", tipo: "auto", descripcion: "", cantidad: 4, precio: 14000,  displonible: true, img: `./assets/imagenVacia.jpg`},
+    {id: 2,marca: "Bridgestone", modelo:"Turanza ER300", medida:"205/55R16", tipo: "auto", descripcion: "", cantidad: 8, precio: 21000, displonible: true, img: `./assets/imagenVacia.jpg`},
+    {id: 3,marca: "Firestone", modelo:"FH900", medida:"195/65R15", tipo: "auto", descripcion: "", cantidad: 2, precio: 21000, displonible: true, img: `./assets/imagenVacia.jpg`},
+    {id: 4,marca: "Bridgestone", modelo:"H/T 684", medida:"265/65R17", tipo: "camioneta", descripcion: "", cantidad: 6, precio: 45000, displonible: true, img: `./assets/imagenVacia.jpg`},
+    {id: 5,marca: "Firestone", modelo:"CV 5000", medida:"195/75R16", tipo: "camioneta", descripcion: "", cantidad: 10, precio: 40000, displonible: true, img: `./assets/imagenVacia.jpg`}, 
+]
+
+ const listaProductos = document.getElementsByClassName("wrapProducts")
+
+ stockNeumaticos.forEach((neumatico) =>{
+    const div = document.createElement(`div`)
+    div.classList.add(`neumatico`)
+    div.innerHTML = `
+    <img src="${neumatico.img}" alt="" >
+    <h3> ${neumatico.marca} - ${neumatico.modelo} - ${neumatico.medida} </h3>
+    <p>Precio:$ ${neumatico.precio}</p>
+    <button id="agregar${neumatico.id}" class= "agregarCarrito"> Agregar</button>
+    `
+    listaProductos.appenChild(div)
+ })
