1c91c7fb805063ff3a2ad7d255f4faa76e0dd840
jesusdavid20 <jesusacostadd24@gmail.com>
2022-09-21 00:12:39 -0300

    Adding modal and other things

--- js/app.js
+++ js/app.js
@@ -134,7 +134,8 @@ let stockNeumaticos = [
     {id: 2,marca: "Bridgestone", modelo:"Turanza ER300", medida:"205/55R16", tipo: "auto", descripcion: "", cantidad: 8, precio: 21000, displonible: true, img: `./assets/imagenVacia.jpg`},
     {id: 3,marca: "Firestone", modelo:"FH900", medida:"195/65R15", tipo: "auto", descripcion: "", cantidad: 2, precio: 21000, displonible: true, img: `./assets/imagenVacia.jpg`},
     {id: 4,marca: "Bridgestone", modelo:"H/T 684", medida:"265/65R17", tipo: "camioneta", descripcion: "", cantidad: 6, precio: 45000, displonible: true, img: `./assets/imagenVacia.jpg`},
-    {id: 5,marca: "Firestone", modelo:"CV 5000", medida:"195/75R16", tipo: "camioneta", descripcion: "", cantidad: 10, precio: 40000, displonible: true, img: `./assets/imagenVacia.jpg`}, 
+    {id: 5,marca: "Firestone", modelo:"CV 5000", medida:"195/75R16", tipo: "camioneta", descripcion: "", cantidad: 10, precio: 40000, displonible: true, img: `./assets/imagenVacia.jpg`},
+    {id: 6,marca: "Bridgestone", modelo:"M/T 674", medida:"265/65R17", tipo: "camioneta", descripcion: "", cantidad: 12, precio: 75000, displonible: true, img: `./assets/imagenVacia.jpg`}, 
 ]
 
 let carrito =[]
@@ -146,7 +147,8 @@ let carrito =[]
     div.classList.add(`neumatico`)
     div.innerHTML = `
     <img src="${neumatico.img}" alt="" >
-    <h3> ${neumatico.marca} - ${neumatico.modelo} - ${neumatico.medida} </h3>
+    <h3> ${neumatico.marca} ${neumatico.modelo}</h3>
+    <h3> ${neumatico.medida}</h3>
     <p>Precio:$ ${neumatico.precio}</p>
     <button id="agregar${neumatico.id}" class= "agregarCarrito"> Agregar</button>
     `
